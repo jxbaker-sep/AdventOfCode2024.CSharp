9f5a5ffde9bb9fd6e8d7ed063584a221fde84830
Jake Baker <SEP.Baker@fronius.com>
2024-12-03 00:31:46 -0500

    stuff

--- Day03.cs
+++ Day03.cs
@@ -27,19 +27,14 @@ public class Day03
 
   private long SumMuls(string arg1)
   {
-    var c = arg1.ToCharArray();
-    var i = 0;
-    var mulp = P.Sequence(P.Digit.Range(1,3).Join().After(P.String("mul(")),
-      P.Digit.Range(1,3).Join().Between(P.String(","), P.String(")")))
-      .Select(it => Convert.ToInt64(it.First) * Convert.ToInt64(it.Second));
-    long sum = 0;
-    while (i < c.Length) {
-      if (mulp.Parse(c, i) is ParseSuccess<long> s) {
-        sum += s.Value;
-        i = s.Position;
-      } else { i += 1; }
-    }
-    return sum;
+    return (
+      P.Sequence(
+        P.Digit.Range(1, 3).Join().After(P.String("mul(")),
+        P.Digit.Range(1, 3).Join().Between(P.String(","), P.String(")"))
+      )
+      .Select(it => Convert.ToInt64(it.First) * Convert.ToInt64(it.Second))
+      | P.Any.Select(_ => 0L)).Star().Select(it => it.Sum()
+    ).Parse(arg1);
   }
 
   private long SumMuls2(string arg1)
@@ -47,25 +42,30 @@ public class Day03
     var enabled = true;
     var c = arg1.ToCharArray();
     var i = 0;
-    var mulp = P.Sequence(P.Digit.Range(1,3).Join().After(P.String("mul(")),
-      P.Digit.Range(1,3).Join().Between(P.String(","), P.String(")")))
+    var mulp = P.Sequence(P.Digit.Range(1, 3).Join().After(P.String("mul(")),
+      P.Digit.Range(1, 3).Join().Between(P.String(","), P.String(")")))
       .Select(it => Convert.ToInt64(it.First) * Convert.ToInt64(it.Second));
     var dop = P.String("do()");
     var dontp = P.String("don't()");
     long sum = 0;
-    while (i < c.Length) {
-      if (dop.Parse(c, i) is ParseSuccess<string> s) {
+    while (i < c.Length)
+    {
+      if (dop.Parse(c, i) is ParseSuccess<string> s)
+      {
         enabled = true;
         i = s.Position;
       }
-      else if (dontp.Parse(c, i) is ParseSuccess<string> s2) {
+      else if (dontp.Parse(c, i) is ParseSuccess<string> s2)
+      {
         enabled = false;
         i = s2.Position;
       }
-      else if (mulp.Parse(c, i) is ParseSuccess<long> s3) {
+      else if (mulp.Parse(c, i) is ParseSuccess<long> s3)
+      {
         sum += enabled ? s3.Value : 0;
         i = s3.Position;
-      } else { i += 1; }
+      }
+      else { i += 1; }
     }
     return sum;
   }
