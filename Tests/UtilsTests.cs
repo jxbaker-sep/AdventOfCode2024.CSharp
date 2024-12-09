ee18833004d3d61dc0b5e249ac7f9cb859c839f0
Jake Baker <SEP.Baker@fronius.com>
2024-12-09 10:22:10 -0500

    stuff

--- Tests/UtilsTests.cs
+++ Tests/UtilsTests.cs
@@ -2,57 +2,61 @@ using AdventOfCode2024.CSharp.Utils;
 using FluentAssertions;
 using Utils;
 
-namespace AdventOfCode2024.CSharp.Tests;
-
-public class UtilsTests
+namespace AdventOfCode2024.CSharp.Tests
 {
-  [Fact]
-  public void WindowTests()
+  public class UtilsTests
   {
-    var input = new List<int> { 1, 2, 3 };
-    input.Windows(0).Should().BeEquivalentTo(new List<List<int>>{
-      new List<int>{}, new List<int>{}, new List<int>{}
-    });
+    [Fact]
+    public void WindowTests()
+    {
+      List<int> input = [1, 2, 3];
+      input.Windows(0).Should().BeEquivalentTo(new List<List<int>>{
+        new() {}, new() {}, new() {}
+      });
 
-    input.Windows(1).Should().BeEquivalentTo(new List<List<int>>{
-      new List<int>{1}, new List<int>{2}, new List<int>{3}
-    });
+      input.Windows(1).Should().BeEquivalentTo(new List<List<int>>{
+        new() {1}, new() {2}, new() {3}
+      });
 
-    input.Windows(2).Should().BeEquivalentTo(new List<List<int>>{
-      new List<int>{1,2}, new List<int>{2,3}
-    });
+      input.Windows(2).Should().BeEquivalentTo(new List<List<int>>{
+        new() {1,2}, new() {2,3}
+      });
 
-    input.Windows(3).Should().BeEquivalentTo(new List<List<int>>{
-      new List<int>{1,2,3}
-    });
+      input.Windows(3).Should().BeEquivalentTo(new List<List<int>>{
+        new() {1,2,3}
+      });
 
-    input = new List<int> { 1, 2, 3, 4, 5 };
-    input.Windows(3).Should().BeEquivalentTo(new List<List<int>>{
-      new List<int>{1,2,3},
-      new List<int>{2,3,4},
-      new List<int>{3,4,5},
-    });
+      input = [1, 2, 3, 4, 5];
+      input.Windows(3).Should().BeEquivalentTo(new List<List<int>>{
+        new() {1,2,3},
+        new() {2,3,4},
+        new() {3,4,5},
+      });
+    }
+
+    [Fact]
+    public void PairTest()
+    {
+      int[] v = [1, 2, 3, 4, 5];
+      v.ToList().Pairs().Should().BeEquivalentTo(new (int, int)[] {
+        (1,2), (1,3), (1,4), (1,5),
+        (2,3), (2,4), (2,5),
+        (3,4), (3,5),
+        (4,5)
+      });
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(2, 3)]
+    [InlineData(3, 6)]
+    [InlineData(4, 10)]
+    [InlineData(5, 15)]
+    [InlineData(6, 21)]
+    public void TriangleTest(long x, long expected)
+    {
+      MathUtils.Triangle(x).Should().Be(expected);
+    }
   }
-
-  [Fact]
-  public void PairTest()
-  {
-    var v = new[] { 1, 2, 3, 4, 5 };
-    v.ToList().Pairs().Should().BeEquivalentTo(new (int, int)[] {
-      (1,2), (1,3), (1,4), (1,5),
-      (2,3), (2,4), (2,5),
-      (3,4), (3,5),
-      (4,5)
-    });
-  }
-
-  [Theory]
-  [InlineData(0, 0)]
-  [InlineData(1, 1)]
-  [InlineData(2, 3)]
-  [InlineData(3, 6)]
-  [InlineData(4, 10)]
-  [InlineData(5, 15)]
-  [InlineData(6, 21)]
-  public void TriangleTest(long x, long expected) => MathUtils.Triangle(x).Should().Be(expected);
 }
