4c9169f6e6e89f79c88314cde94ebf854042acfa
Jake Baker <SEP.Baker@fronius.com>
2024-12-09 01:01:44 -0500

    day09.2

--- Day09.cs
+++ Day09.cs
@@ -11,7 +11,7 @@ public class Day09
   [Theory]
   [InlineData("Day09.Sample", 1928)]
   [InlineData("Day09", 6200294120911L)]
-  public void Part1(string file, int expected)
+  public void Part1(string file, long expected)
   {
     var input = FormatInput(AoCLoader.LoadLines(file));
     var compressed = Compress1(input);
@@ -21,10 +21,10 @@ public class Day09
   [Theory]
   [InlineData("Day09.Sample", 2858)]
   [InlineData("Day09", 6227018762750L)]
-  public void Part2(string file, int expected)
+  public void Part2(string file, long expected)
   {
     var input = FormatInput(AoCLoader.LoadLines(file));
-    var compressed = Compress2(input, input.Max());
+    var compressed = Compress2(input);
     Checksum(compressed).Should().Be(expected);
   }
 
@@ -52,41 +52,49 @@ public class Day09
     return input;
   }
 
-  private List<long> Compress2(List<long> input, long id)
+  private List<long> Compress2(List<long> input)
   {
-    var result = input.ToList();
+    Dictionary<long, (int index, int length)> files = [];
+    HashSet<(int index, int length)> free = [];
 
-    var start = input.IndexOf(id);
-    var end = input.LastIndexOf(id);
-    var length = end - start + 1;
-
-    var head = 0;
-    var freespan = 0;
-    var freespanstart = -1;
-    while (freespan < length && head < start)
+    var i = 1;
+    var current = (index: 0, length: 1, id: input[0]);
+    while (i < input.Count)
     {
-      if (input[head] == -1)
-      {
-        freespan += 1;
-        if (freespanstart == -1) freespanstart = head;
+      if (input[i] == current.id) {
+        current = (current.index, length: current.length + 1, current.id);
+        i ++;
+        continue;
       }
-      else 
+      if (current.id == -1) free.Add((current.index, current.length));
+      else files[current.id] = (current.index, current.length);
+      current = (index: i, length: 1, id: input[i]);
+      i++;
+    }
+    // make sure to add to the last one
+    if (current.id == -1) free.Add((current.index, current.length));
+    else files[current.id] = (current.index, current.length);
+
+    for(var id = input.Max(); id >= 0; id--)
+    {
+      var file = files[id];
+      var freeSpans = free.Where(it => it.length >= file.length && it.index < file.index).OrderBy(it => it.index).Take(1).ToList();
+      if (freeSpans.Count == 0) continue;
+      var freeSpan = freeSpans[0];
+      for(var x = 0; x < file.length; x++)
       {
-        freespan = 0;
-        freespanstart = -1;
+        input[freeSpan.index + x] = id;
+        input[file.index + x] = -1;
       }
-      head++;
+      free.Remove(freeSpan);
+      if (freeSpan.length > file.length)
+      {
+        free.Add((freeSpan.index + file.length, freeSpan.length - file.length));
+      }
+      free.Add((file.index, file.length));
     }
 
-    if (freespan == length) {
-      for(var i = 0 ; i < length; i++) {
-        input[start + i] = -1;
-        input[freespanstart + i] = id;
-      }
-    }
-
-    if (id == 0) return input;
-    return Compress2(input, id - 1);
+    return input;
   }
 
   private static List<long> FormatInput(List<string> input)
