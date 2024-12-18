248eda10af3699e59db337d1089e3f23815338cb
Jake Baker <SEP.Baker@fronius.com>
2024-12-18 12:01:26 -0500

    improved.binary.search

---
+++ Utils/MiscUtils.cs
@@ -0,0 +1,25 @@
+namespace AdventOfCode2024.CSharp.Utils;
+
+public static class MiscUtils
+{
+  public static int? BinarySearch(int maxValue, Func<int, bool> action) {
+    int? knownMin = null;
+    int? knownMax = null;
+
+    while (true) {
+      var useMin = knownMin ?? 0;
+      var useMax = knownMax ?? maxValue + 1;
+      int attempt = (useMin + useMax) / 2;
+      if (action(attempt)) {
+        if (attempt == 0) return 0;
+        if (knownMin == attempt - 1) return attempt;
+        knownMax = attempt;
+      } else {
+        if (attempt == maxValue) return null;
+        if (knownMax == attempt + 1) return knownMax;
+        knownMin = attempt;
+      }
+    }
+    throw new ApplicationException();
+  }
+}
